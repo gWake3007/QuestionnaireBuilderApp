67ee8fea062821e433dd2ffb3cead9d26f1fcde7
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-03-26 00:11:42 +0100

    123

--- src/pages/Quiz_Catalog/Quiz_Catalog.jsx
+++ src/pages/Quiz_Catalog/Quiz_Catalog.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchQuizzes } from '../../redux/quiz/operations.js';
 import { Link } from 'react-router-dom';
-// import Quiz_List from '../../compponents/Quiz_List/Quiz_List.jsx';
+import Quiz_List from '../../compponents/Quiz_List/Quiz_List.jsx';
 
 const Quiz_Catalog = () => {
   const dispatch = useDispatch();
