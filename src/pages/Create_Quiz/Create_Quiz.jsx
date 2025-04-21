9fb5293175ac04645912486821678e8b649caa89
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-04-21 19:35:13 +0200

    123

--- src/pages/Create_Quiz/Create_Quiz.jsx
+++ src/pages/Create_Quiz/Create_Quiz.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { nanoid } from 'nanoid';
 import Quiz_Form from '../../compponents/Quiz_Form/Quiz_Form.jsx';
 import { addQuiz } from '../../redux/quiz/operations.js';
 
@@ -9,20 +10,25 @@ const CreateQuizPage = () => {
   const navigate = useNavigate();
 
   const handleSubmit = (values, { resetForm }) => {
-    // Валідація чи є питання
     if (values.questions.length === 0) {
       alert('Додайте хоча б одне питання');
       return;
     }
 
-    dispatch(addQuiz(values));
+    const newQuiz = {
+      ...values,
+      id: nanoid(),
+      createdAt: new Date().toISOString(),
+    };
+
+    dispatch(addQuiz(newQuiz));
     navigate('/');
     resetForm();
   };
 
   return (
     <div>
-      <Link to={'/'}>back</Link>
+      <Link to="/">← Назад</Link>
       <h2>Створити новий квіз</h2>
       <Quiz_Form
         initialValues={{
